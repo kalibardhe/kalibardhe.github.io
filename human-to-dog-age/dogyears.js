a889fdac6eabedbc199e230c9eec913cb16affb5
Paulo Cavalini <55258447+kalibardhe@users.noreply.github.com>
2022-12-20 19:44:38 +0000

    First commit of dogyears

---
+++ human-to-dog-age/dogyears.js
@@ -0,0 +1,23 @@
+// my age in years
+let myAge = 10;
+
+// first two years of a dog
+let earlyYears = 2;
+
+// multiply early years by 10.5 and update the variable
+earlyYears *= 10.5;
+
+// later years of a dog
+let laterYears = myAge - 2;
+
+// calculate the number of dog years accounted for by the later years
+laterYears *= 4;
+
+// add early years to later years to get own age in dog years
+myAgeInDogYears = earlyYears + laterYears;
+
+// store my name into a variable and convert it to lower case
+let myName = 'John Doe'.toLowerCase()
+
+// display my name and age in dog years
+console.log(`My name is ${myName}. I am ${myAge} years old in human years which is ${myAgeInDogYears} years old in dog years.`)
